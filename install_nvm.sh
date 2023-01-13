fd136bfce3f1c0a0a57fa4d3402bab76299878a2
Mudrekh Goderya <mudrekh@gmail.com>
2023-01-13 14:38:15 -0600

    fix nvm install

--- install_nvm.sh
+++ install_nvm.sh
@@ -4,7 +4,7 @@
 NVM_URL="https://raw.githubusercontent.com/creationix/nvm/master/install.sh"
 
 # nvm, node and modules
-wget -qO- $NVM_URL | zsh
+wget -qO- $NVM_URL | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
