b34f9491fb432a98ae89422ea83ac83dff331a2a
Mudrekh Goderya <mudrekh@gmail.com>
2023-08-16 17:45:30 -0500

    Update docker script for ubuntu specific install

--- docker.sh
+++ docker.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
-curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
-sudo bash /tmp/get-docker.sh
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker $USER
+newgrp docker
